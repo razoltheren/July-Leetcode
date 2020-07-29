0cfcfeb9b05d5df869c7aff3cc8423698fb26b3d
razoltheren <ishitabatra0809@gmail.com>
2020-07-29 21:36:23 +0530

    Day29

---
+++ Day29.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int sold = INT_MIN;
+        int held = INT_MIN;
+        int reset = 0;
+        for (auto price : prices) {
+            int preSold = sold;
+            sold = held+price;
+            held = max(held, reset - price);
+            reset = max(reset, preSold);
+        }
+        return max(sold, reset);
+    }
+};
